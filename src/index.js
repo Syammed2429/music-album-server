00ab7308db6dcfdf096ff8f91b67832ecf970355
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-14 17:08:37 +0530

    renamed from musiccontroller to albumcontroller

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 
-const musicController = require('./controller/musicController')
+const albumController = require('./controller/albumController')
 
 const port = 2924;
 
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-app.use('', musicController)
+app.use('', albumController)
 
 module.exports = {
     port,
