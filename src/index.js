632d88d0a79762a2f909b1bbe9da53a1e68c9711
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-14 17:14:06 +0530

    imported songs controller in index and created a end point

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors')
 
 const albumController = require('./controller/albumController')
+const songsController = require('./controller/songsController')
 
 const port = 2924;
 
@@ -10,7 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-app.use('', albumController)
+app.use('/albums', albumController)
+app.use('/songs', songsController)
 
 module.exports = {
     port,
