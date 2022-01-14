94617539f8baeeca99a62bd963475595750534a1
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-14 17:04:14 +0530

    imported musiccontroller and added a route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors')
 
+const musicController = require('./controller/musicController')
+
 const port = 2924;
 
 const app = express();
@@ -8,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
+app.use('', musicController)
 
 module.exports = {
     port,
