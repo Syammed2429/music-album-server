d1718b261ad1b3fa5a50ea39e3bf560a38960735
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-14 17:05:58 +0530

    added POST for music controller

--- src/controller/musicController.js
+++ src/controller/musicController.js
@@ -7,6 +7,13 @@ const router = express.Router()
 
 //CRUD
 
+//Create
+router.post('', async (req, res) => {
+    const album = await Album.create(req.body);
+
+    return res.status(201).send(album)
+});
+
 //GET 
 router.get('', async (req, res) => {
     return res.status(200).send('Hello')
