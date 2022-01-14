d536bf06a8e9b3073e3c9599ddfd056f2787dc63
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-14 21:39:08 +0530

    completed the coding

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const cors = require('cors')
 const albumController = require('./controller/albumController')
 const songsController = require('./controller/songsController')
 
-const port = 2924;
+const port = process.env.PORT || 2924
+
 
 const app = express();
 
@@ -13,6 +14,10 @@ app.use(express.json())
 
 app.use('/albums', albumController)
 app.use('/songs', songsController)
+
+app.get('/', (req, res) => {
+    return res.status(200).send({ message: 'Hello Welcome to the Music album', getpoints: "Here are getpoint , /albums and /songs" })
+})
 
 module.exports = {
     port,
