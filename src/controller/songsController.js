6fe25a07d90814256465b016a05e50b4dbdba782
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-14 17:12:59 +0530

    build post api

--- src/controller/songsController.js
+++ src/controller/songsController.js
@@ -4,3 +4,14 @@ const express = require('express');
 const Song = require('../models/songsModel');
 
 const router = express.Router()
+
+//CRUD
+
+//POST
+router.post('', async (req, res) => {
+    const song = await Song.create(req.body);
+
+    return res.status(201).send(song);
+})
+
+module.exports = router;
