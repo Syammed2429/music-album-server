90b28d602701184c4a5a8c978aff2665fd2ff499
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-15 09:32:10 +0530

    re-arranged the api get points

--- src/controller/albumController.js
+++ src/controller/albumController.js
@@ -13,6 +13,29 @@ router.post('', async (req, res) => {
     return res.status(201).send(album)
 });
 
+
+// search the songs
+router.get('/search', async (req, res) => {
+    console.log(req.query.q);
+    const songs = await Album.find({ name: req.query.q })
+
+    return res.status(200).send(songs)
+
+})
+
+
+//Get the album for particular artist name
+router.get('/artist', async (req, res) => {
+    try {
+        const albums = await Album.find({ artist_name: req.query.name })
+        if (albums.length === 0) return res.status(404).send("You don't have any albums yet")
+        return res.status(200).send(albums)
+    } catch (e) {
+        console.log('e:', e)
+
+    }
+})
+
 //GET  all albums
 router.get('', async (req, res) => {
     try {
@@ -43,26 +66,8 @@ router.get('/:id', async (req, res) => {
 })
 
 
-//search the songs
-router.get('/search', async (req, res) => {
-    console.log(req.query.q);
-    const songs = await Album.find({ name: req.query.q })
 
-    return res.status(200).send(songs)
 
-})
-
-//Get the album for particular artist name
-router.get('/artist', async (req, res) => {
-    try {
-        const albums = await Album.find({ artist_name: req.query.name })
-        if (albums.length === 0) return res.status(404).send("You don't have any albums yet")
-        return res.status(200).send(albums)
-    } catch (e) {
-        console.log('e:', e)
-
-    }
-})
 
 
 
